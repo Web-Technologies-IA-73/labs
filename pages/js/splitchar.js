bb9bd570fb13b82e0f3a59f6aefa20bc1412ea30
Danylo Shevchenko <danilshevchenko123456@gmail.com>
2021-03-23 08:32:54 +0200

    feat(lab5)

---
+++ pages/js/splitchar.js
@@ -0,0 +1,29 @@
+/**
+ * Splitchar.js
+ * @author emisfera - Razvan B.
+ */
+
+$.fn.splitchar = function() {	
+	$('.splitchar').each(function (index) {
+	    var characters = $(this).text().split("");
+	    $this = $(this);
+	    $this.empty();
+	    $.each(characters, function (i, el) {
+            if ( $this.hasClass("horizontal") ) {
+	    	$this.append("<span class='horizontal' data-content='" + el + "'>" + el + "</span>");
+            }
+            else if ( $this.hasClass("horizontal-tri") ) {
+	    	$this.append("<span class='horizontal-tri' data-content='" + el + "'>" + el + "</span>");
+            }
+            else if ( $this.hasClass("vertical") ) {
+            $this.append("<span class='vertical' data-content='" + el + "'>" + el + "</span>");    
+            }
+            else if ( $this.hasClass("vertical-tri") ) {
+            $this.append("<span class='vertical-tri' data-content='" + el + "'>" + el + "</span>");    
+            }
+            else {
+            $this.append("<span class='horizontal' data-content='" + el + "'>" + el + "</span>");	
+            }            
+	    });
+	});
+};
